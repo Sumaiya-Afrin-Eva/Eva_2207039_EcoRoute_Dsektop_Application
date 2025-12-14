0d922d3a076425791e4291ba74870cf4ee9de057
Sumaiya-Afrin-Eva <sumaiya.afrin.evaa@gmail.com>
2025-12-14 18:11:58 +0600

    Initial Commit - Project Eco-Route

---
+++ src/main/java/com/example/eco_route/HelloApplication.java
@@ -0,0 +1,22 @@
+package com.example.eco_route;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("home_view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("EcoRoute!");
+        stage.setScene(scene);
+        stage.setMaximized(true);
+        stage.show();
+    }
+    public static void main(String[] args) { launch(args);
+    }
+}
